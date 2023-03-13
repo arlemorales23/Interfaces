2d549b7e111d5d9d89bebc192ecb75d7871b708f
Arleth64 <87079423+arleth64@users.noreply.github.com>
2023-03-13 07:03:42 -0500

    Updatin things

---
+++ src/ejemplo3/In2.java
@@ -0,0 +1,23 @@
+package ejemplo3;
+
+interface In2 {
+
+    // Here new comentary
+    // Other comentary
+    //New comentary
+
+        final int a = 10;
+        default void display()  {
+            System.out.println("hello");
+        }
+    }
+
+    // A class that implements the interface.
+    class TestClass implements In2 {
+        // Driver Code
+        public static void main (String[] args)
+        {
+            TestClass t = new TestClass();
+            t.display();
+        }
+    }
